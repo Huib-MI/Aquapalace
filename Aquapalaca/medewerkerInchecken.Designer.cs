c69efe102a84e9b160583a6b605acc80ae82abc7
lucad <lucadehaas1@gmail.com>
2025-09-16 11:45:04 +0200

    buttons op medewerker pagina toegevoegd

---
+++ Aquapalaca/medewerkerInchecken.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Aquapalaca
+{
+    partial class medewerkerInchecken
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTerug = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTerug
+            // 
+            this.btnTerug.Location = new System.Drawing.Point(587, 315);
+            this.btnTerug.Name = "btnTerug";
+            this.btnTerug.Size = new System.Drawing.Size(137, 93);
+            this.btnTerug.TabIndex = 0;
+            this.btnTerug.Text = "Terug";
+            this.btnTerug.UseVisualStyleBackColor = true;
+            this.btnTerug.Click += new System.EventHandler(this.btnTerug_Click);
+            // 
+            // medewerkerInchecken
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTerug);
+            this.Name = "medewerkerInchecken";
+            this.Text = "medewerkerInchecken";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTerug;
+    }
+}
---
+++ Aquapalaca/medewerkerKlanten.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aquapalaca
+{
+    public partial class medewerkerKlanten : Form
+    {
+        public medewerkerKlanten()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            medewerkerPagina MedewerkerPagina = new medewerkerPagina();
+            MedewerkerPagina.Show();
+            this.Hide();
+        }
+    }
+}

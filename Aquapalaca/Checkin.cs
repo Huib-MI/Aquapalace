f6c22e8d6ab3cc9cedccede062ffacb2abcf1633
huib minderhoud <huibminderhout@gmail.com>
2025-09-26 10:55:57 +0200

    Commit

--- Aquapalaca/Checkin.cs
+++ Aquapalaca/Checkin.cs
@@ -2,77 +2,93 @@
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aquapalaca
 {
     class Checkin
     {
         public int Id;
-        public string SubscriptionId;
-        public string CustomerId;
-        public DateTime Time;
-        public string Method;
+        public int SubscriptieId;
+        public int KlantId;
+        public DateTime CheckinTijd;
+        public string CheckinMethode;
 
         public static List<Checkin> getCheckIn()
         {
             List<Checkin> checkinlist = new List<Checkin>();
 
-            MySqlConnection con = Databases.start();
-            con.Open();
-
-            MySqlCommand myCommand = new MySqlCommand();
-            myCommand.Connection = con;
-            myCommand.CommandText = @"SELECT * FROM checkins";
-
-            MySqlDataReader reader = myCommand.ExecuteReader();
-
-            while (reader.Read())
+            using (MySqlConnection con = Databases.start())
             {
-                Checkin checkinobject = new Checkin();
-                checkinobject.Id = Convert.ToInt32((reader["checkin_id"]));
-                checkinobject.SubscriptionId = Convert.ToString(reader["checkin_subscription_id"]);
-                checkinobject.CustomerId = Convert.ToString(reader["checkin_customer_id"]);
-                checkinobject.Time = Convert.ToDateTime(reader["checkin_time"]);
-                checkinobject.Method = Convert.ToString(reader["checkin_method"]);
-                checkinlist.Add(checkinobject);
-            }
+                con.Open();
+                MySqlCommand myCommand = new MySqlCommand("SELECT * FROM checkins", con);
+                MySqlDataReader reader = myCommand.ExecuteReader();
 
-            con.Close();
+                while (reader.Read())
+                {
+                    Checkin checkinobject = new Checkin();
+                    checkinobject.Id = Convert.ToInt32(reader["checkin_id"]);
+                    checkinobject.SubscriptieId = Convert.ToInt32(reader["checkin_subscription_id"]);
+                    checkinobject.KlantId = Convert.ToInt32(reader["checkin_customer_id"]);
+                    checkinobject.CheckinTijd = Convert.ToDateTime(reader["checkin_time"]);
+                    checkinobject.CheckinMethode = Convert.ToString(reader["checkin_method"]);
+                    checkinlist.Add(checkinobject);
+                }
+            }
 
             return checkinlist;
         }
 
-
         public void Inchecken()
         {
-            MySqlConnection con = Databases.start();
-            con.Open();
-            MySqlCommand myCommand = new MySqlCommand();
-            myCommand.Connection = con;
-            myCommand.CommandText = @"UPDATE checkins 
-                                        SET 
-                                        checkin_subscription_id = @SubscriptionId,
-                                        checkin_customer_id = @CustomerId,
-                                        checkin_time = @Time,
-                                        checkin_method = @Method
-                                        WHERE
-                                        checkin_id = @Id;";
-            myCommand.Parameters.AddWithValue("@SubscriptionId", this.SubscriptionId);
-            myCommand.Parameters.AddWithValue("@CustomerId", this.CustomerId);
-            myCommand.Parameters.AddWithValue("@Time", this.Time);
-            myCommand.Parameters.AddWithValue("@Method", this.Method);
-            myCommand.Parameters.AddWithValue("@Id", this.Id);
-            myCommand.ExecuteNonQuery();
-            myCommand.Dispose();
-            con.Close();
+            using (MySqlConnection con = Databases.start())
+            {
+                con.Open();
+                MySqlCommand myCommand = new MySqlCommand(@"
+                    UPDATE checkins
+                    SET 
+                        checkin_subscription_id = @SubscriptieId,
+                        checkin_customer_id = @KlantId,
+                        checkin_time = @CheckinTijd,
+                        checkin_method = @CheckinMethode
+                    WHERE checkin_id = @Id;", con);
+
+                myCommand.Parameters.AddWithValue("@SubscriptieId", this.SubscriptieId);
+                myCommand.Parameters.AddWithValue("@KlantId", this.KlantId);
+                myCommand.Parameters.AddWithValue("@CheckinTijd", this.CheckinTijd);
+                myCommand.Parameters.AddWithValue("@CheckinMethode", this.CheckinMethode);
+                myCommand.Parameters.AddWithValue("@Id", this.Id);
+
+                myCommand.ExecuteNonQuery();
+            }
         }
 
+        public void Insert()
+        {
+            using (MySqlConnection con = Databases.start())
+            {
+                con.Open();
+                string sql = @"
+                    INSERT INTO checkins (
+                        checkin_subscription_id,
+                        checkin_customer_id,
+                        checkin_time,
+                        checkin_method
+                    ) VALUES (
+                        @SubscriptieId,
+                        @KlantId,
+                        @CheckinTijd,
+                        @CheckinMethode
+                    );";
 
+                using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                {
+                    cmd.Parameters.AddWithValue("@SubscriptieId", this.SubscriptieId);
+                    cmd.Parameters.AddWithValue("@KlantId", this.KlantId);
+                    cmd.Parameters.AddWithValue("@CheckinTijd", this.CheckinTijd);
+                    cmd.Parameters.AddWithValue("@CheckinMethode", this.CheckinMethode);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
-
-
